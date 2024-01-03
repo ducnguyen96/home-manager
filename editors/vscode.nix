d1a45e97981a2c0a405ba18ed0d8bff41d43fd33
ducnguyen96 <levinguyen.dl@gmail.com>
2024-01-03 15:17:23 +0700

    fix vscode marketplace extension

--- editors/vscode.nix
+++ editors/vscode.nix
@@ -1,7 +1,13 @@
-{ pkgs, inputs, ... }:
+{ pkgs, bultins, ... }:
 
-let 
-  marketplace-extensions = with inputs.nix-vscode-extensions.extensions.${pkgs.system}.vscode-marketplace; [
+let
+  nix-vscode-extensions = (import (builtins.fetchGit {
+      url = "https://github.com/nix-community/nix-vscode-extensions";
+      ref = "refs/heads/master";
+      rev = "20f496592b1dd2bb1ffeae59de2619ca3ec8c1f8";
+    })).extensions.${builtins.currentSystem};
+
+  marketplace-extensions = with nix-vscode-extensions.vscode-marketplace; [
     rvest.vs-code-prettier-eslint
   ];
 in 
@@ -20,6 +26,7 @@ in
     userSettings = {
       github.copilot.enable."*" = true;
       security.workspace.trust.enabled = false;
+      window.menuBarVisibility =  "toggle";
 
       "[css]".editor.defaultFormatter = "esbenp.prettier-vscode";
       "[html]".editor.defaultFormatter = "esbenp.prettier-vscode";
